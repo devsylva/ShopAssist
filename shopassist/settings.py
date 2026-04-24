4a480a77a28dbca18043bf3c9fdc619c5d237cb9
Apple MacBook Pro <applemacbookpro@Apples-MacBook-Pro.local>
2026-04-25 00:55:53 +0100

    bug fix: csrf

--- shopassist/settings.py
+++ shopassist/settings.py
@@ -15,6 +15,8 @@ DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
 
+CSRF_TRUSTED_ORIGINS = os.environ.get('CSRF_TRUSTED_ORIGINS', '').split(',') if os.environ.get('CSRF_TRUSTED_ORIGINS') else []
+
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
